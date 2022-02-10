3c54f810e12a0b3fd267f2e0b0c504d6cab70d8f
José <josejuniordsc@gmail.com>
2022-02-09 23:19:14 -0300

    refactor: files disposition

--- packages/stream_chat/lib/src/core/platform_detector/platform_detector_stub.dart
+++
@@ -1,6 +0,0 @@
-import 'package:stream_chat/src/core/platform_detector/platform_detector.dart';
-
-/// Stub implementation
-PlatformType get currentPlatform {
-  throw UnimplementedError();
-}
--- packages/stream_chat/lib/src/core/platform_detector/platform_detector_web.dart
+++
@@ -1,4 +0,0 @@
-import 'package:stream_chat/src/core/platform_detector/platform_detector.dart';
-
-/// Version running on web
-PlatformType get currentPlatform => PlatformType.web;
